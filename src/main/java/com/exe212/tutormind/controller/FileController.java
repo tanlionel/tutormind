d45829e1e67abc727cab4491da3dee7c4e4ab2b0
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-05-29 14:08:23 +0700

    done upload

---
+++ src/main/java/com/exe212/tutormind/controller/FileController.java
@@ -0,0 +1,25 @@
+package com.exe212.tutormind.controller;
+
+import com.exe212.tutormind.service.service_interface.FileService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/files")
+@RequiredArgsConstructor
+public class FileController {
+
+
+    private final FileService fileService;
+
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public String addFile(@RequestParam("files") MultipartFile files) throws BadRequestException {
+        return fileService.uploadFiles(files);
+    }
+}
